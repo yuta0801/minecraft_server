62ed3acab7a4aabe6697674e2076e969c7a1e7a8
yuta0801 <yuta030801@gmail.com>
2020-08-03 20:14:02 +0900

    move reader functions to module

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,24 @@
+mod reader;
+
 use std::io;
-use std::io::{BufReader, Read};
+use std::io::BufReader;
 use std::net::{TcpListener, TcpStream};
-use std::string;
-
-fn read_var_int(reader: &mut BufReader<&TcpStream>) -> io::Result<i32> {
-    const MORE_FLAG: u8 = 0b10000000;
-    let mut int = 0i32;
-    let mut num_read = 0;
-
-    loop {
-        let mut buf = [0];
-        reader.read_exact(&mut buf)?;
-        let byte = buf[0];
-
-        int += ((byte & !MORE_FLAG) as i32) << (7 * num_read);
-
-        num_read += 1;
-        if num_read > 5 {
-            return Err(io::Error::new(io::ErrorKind::Other, "VarInt is too big"));
-        }
-
-        if byte & MORE_FLAG == 0 {
-            break;
-        }
-    }
-
-    Ok(int)
-}
-
-// TODO: range check
-fn read_string(
-    reader: &mut BufReader<&TcpStream>,
-) -> io::Result<Result<String, string::FromUtf8Error>> {
-    let len = read_var_int(reader)? as usize;
-    let mut buf = vec![0; len];
-    reader.read_exact(&mut buf)?;
-    Ok(String::from_utf8(buf))
-}
-
-fn read_unsigned_short(reader: &mut BufReader<&TcpStream>) -> io::Result<u16> {
-    let mut buf = [0; 2];
-    reader.read_exact(&mut buf)?;
-    Ok(((buf[0] as u16) << 8) + buf[1] as u16)
-}
 
 fn handler(stream: &TcpStream) -> io::Result<()> {
     let mut reader = BufReader::new(stream);
 
-    let _len = read_var_int(&mut reader)?;
-    let packet_id = read_var_int(&mut reader)?;
+    let _len = reader::read_var_int(&mut reader)?;
+    let packet_id = reader::read_var_int(&mut reader)?;
     println!("packet length: {:?}", _len);
     println!("packet id: {:?}", packet_id);
 
     match packet_id {
         // Handshake
         0x00 => {
-            let version = read_var_int(&mut reader)?;
-            let _address = read_string(&mut reader)?.unwrap_or("invalid".to_string());
-            let _port = read_unsigned_short(&mut reader)?;
-            let state = read_var_int(&mut reader)?;
+            let version = reader::read_var_int(&mut reader)?;
+            let _address = reader::read_string(&mut reader)?.unwrap_or("invalid".to_string());
+            let _port = reader::read_unsigned_short(&mut reader)?;
+            let state = reader::read_var_int(&mut reader)?;
             println!("packet: handshake");
             println!("protocol version: {}", version);
             println!("server address: {}", _address);
---
+++ src/reader.rs
@@ -0,0 +1,45 @@
+use std::io;
+use std::io::{Read, BufReader};
+use std::net::TcpStream;
+use std::string;
+
+pub fn read_var_int(reader: &mut BufReader<&TcpStream>) -> io::Result<i32> {
+    const MORE_FLAG: u8 = 0b10000000;
+    let mut int = 0i32;
+    let mut num_read = 0;
+
+    loop {
+        let mut buf = [0];
+        reader.read_exact(&mut buf)?;
+        let byte = buf[0];
+
+        int += ((byte & !MORE_FLAG) as i32) << (7 * num_read);
+
+        num_read += 1;
+        if num_read > 5 {
+            return Err(io::Error::new(io::ErrorKind::Other, "VarInt is too big"));
+        }
+
+        if byte & MORE_FLAG == 0 {
+            break;
+        }
+    }
+
+    Ok(int)
+}
+
+// TODO: range check
+pub fn read_string(
+    reader: &mut BufReader<&TcpStream>,
+) -> io::Result<Result<String, string::FromUtf8Error>> {
+    let len = read_var_int(reader)? as usize;
+    let mut buf = vec![0; len];
+    reader.read_exact(&mut buf)?;
+    Ok(String::from_utf8(buf))
+}
+
+pub fn read_unsigned_short(reader: &mut BufReader<&TcpStream>) -> io::Result<u16> {
+    let mut buf = [0; 2];
+    reader.read_exact(&mut buf)?;
+    Ok(((buf[0] as u16) << 8) + buf[1] as u16)
+}
