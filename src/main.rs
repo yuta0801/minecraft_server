12e055ba0782d90c204accc031a54bcf5f7741d3
yuta0801 <yuta030801@gmail.com>
2020-08-03 12:18:17 +0900

    tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+
+fn handler(mut stream: &TcpStream) -> io::Result<()> {
+    let mut buf = [0; 1024];
+
+    loop {
+        println!("read");
+        let n = stream.read(&mut buf)?;
+        println!("{:?}", &buf[0..128]);
+        if n == 0 {
+            println!("end");
+            break;
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:25565").expect("failed to bind");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => handler(&stream).unwrap_or_else(|e| println!("{:?}", e)),
+            Err(e) => {
+                println!("error: {}", e);
+                continue;
+            }
+        };
+    }
 }
